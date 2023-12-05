b3a86c13370d335ba7ff1f9392db7b2693b5d881
Dima Pelyp <dpelip03@gmail.com>
2023-12-05 04:14:36 +0100

    test API server completion
    for LogIn form
    contacts
    email drafts

--- server/appRoutes.js
+++ server/appRoutes.js
@@ -3,3 +3,286 @@ const {app} = require('./app.js')
 app.get('/', (req, res)=>{
     res.send('<h1>Your server works fine</h1>')
 })
+
+app.get('/api/mail-templates', (req, res)=>{
+    res.send(JSON.stringify({
+        id: 1,
+        name: "reminder",
+        googleDriveId: "Qwwkjr54-fdkjg"
+    }))
+})
+
+app.get('/api/autorize', (req,res)=>{
+    if(req.query.password==="admin"
+    && req.query.email==="admin") {
+        res.send(JSON.stringify({
+            valid: true
+        }))
+    }
+    else {
+        res.send(JSON.stringify({
+            valid: false
+        }))
+    }
+})
+
+app.get("/api/contacts", (req,res)=>{
+    res.send(JSON.stringify([{
+        "id": 1,
+        "firstName": "Alice",
+        "lastName": "Johnson",
+        "email": "alice@example.com",
+        "age": 30,
+        "country": "Canada",
+        "timezone": "GMT-04:00",
+        "sourceOfReferral": "Website",
+        "eduQuestSelectedDateTime": "2023-10-03T15:45:00Z",
+        "eduQuestDecision": "Pending"
+      },
+      {
+        "id": 2,
+        "firstName": "Bob",
+        "lastName": "Smith",
+        "email": "bob@example.com",
+        "age": 28,
+        "country": "United Kingdom",
+        "timezone": "GMT+00:00",
+        "sourceOfReferral": "Email",
+        "eduQuestSelectedDateTime": "2023-10-03T14:15:00Z",
+        "eduQuestDecision": "Rejected"
+      },
+      {
+        "id": 4,
+        "firstName": "Bob",
+        "lastName": "Smith",
+        "email": "bob@example.com",
+        "age": 28,
+        "country": "United Kingdom",
+        "timezone": "GMT+00:00",
+        "sourceOfReferral": "Email",
+        "eduQuestSelectedDateTime": "2023-10-03T14:15:00Z",
+        "eduQuestDecision": "Rejected"
+      }
+    ]))
+})
+
+app.post("/api/contacts", (req,res)=>{
+    res.send(JSON.stringify([{
+        "id": 1,
+        "firstName": "Alice",
+        "lastName": "Johnson",
+        "email": "alice@example.com",
+        "age": 30,
+        "country": "Canada",
+        "timezone": "GMT-04:00",
+        "sourceOfReferral": "Website",
+        "eduQuestSelectedDateTime": "2023-10-03T15:45:00Z",
+        "eduQuestDecision": "Pending"
+      },
+      {
+        "id": 2,
+        "firstName": "Bob",
+        "lastName": "Smith",
+        "email": "bob@example.com",
+        "age": 28,
+        "country": "United Kingdom",
+        "timezone": "GMT+00:00",
+        "sourceOfReferral": "Email",
+        "eduQuestSelectedDateTime": "2023-10-03T14:15:00Z",
+        "eduQuestDecision": "Rejected"
+      },
+      {
+        "id": 4,
+        "firstName": "Bob",
+        "lastName": "Smith",
+        "email": "bob@example.com",
+        "age": 28,
+        "country": "United Kingdom",
+        "timezone": "GMT+00:00",
+        "sourceOfReferral": "Email",
+        "eduQuestSelectedDateTime": "2023-10-03T14:15:00Z",
+        "eduQuestDecision": "Rejected"
+      }
+    ]))
+})
+
+app.get("/api/contacts/1", (req,res)=>{
+    res.send(JSON.stringify({
+        "id": 1,
+        "firstName": "Alice",
+        "lastName": "Johnson",
+        "email": "alice@example.com",
+        "age": 30,
+        "country": "Canada",
+        "timezone": "GMT-04:00",
+        "sourceOfReferral": "Website",
+        "eduQuestSelectedDateTime": "2023-10-03T15:45:00Z",
+        "eduQuestDecision": "Pending"
+      }))
+})
+
+app.get("/api/contacts/2", (req,res)=>{
+    res.send(JSON.stringify(
+      {
+        "id": 2,
+        "firstName": "Bob",
+        "lastName": "Smith",
+        "email": "bob@example.com",
+        "age": 28,
+        "country": "United Kingdom",
+        "timezone": "GMT+00:00",
+        "sourceOfReferral": "Email",
+        "eduQuestSelectedDateTime": "2023-10-03T14:15:00Z",
+        "eduQuestDecision": "Rejected"
+      }))
+})
+
+app.get("/api/contacts/4", (req,res)=>{
+    res.send(JSON.stringify(
+      {
+        "id": 4,
+        "firstName": "Bob",
+        "lastName": "Smith",
+        "email": "bob@example.com",
+        "age": 28,
+        "country": "United Kingdom",
+        "timezone": "GMT+00:00",
+        "sourceOfReferral": "Email",
+        "eduQuestSelectedDateTime": "2023-10-03T14:15:00Z",
+        "eduQuestDecision": "Rejected"
+      }))
+})
+
+app.put("/api/contacts/1", (req,res)=>{
+    res.send(JSON.stringify({
+        "id": 1,
+        "firstName": "Alice",
+        "lastName": "Johnson",
+        "email": "alice@example.com",
+        "age": 30,
+        "country": "Canada",
+        "timezone": "GMT-04:00",
+        "sourceOfReferral": "Website",
+        "eduQuestSelectedDateTime": "2023-10-03T15:45:00Z",
+        "eduQuestDecision": "Pending"
+      }))
+})
+
+app.put("/api/contacts/2", (req,res)=>{
+    res.send(JSON.stringify(
+      {
+        "id": 2,
+        "firstName": "Bob",
+        "lastName": "Smith",
+        "email": "bob@example.com",
+        "age": 28,
+        "country": "United Kingdom",
+        "timezone": "GMT+00:00",
+        "sourceOfReferral": "Email",
+        "eduQuestSelectedDateTime": "2023-10-03T14:15:00Z",
+        "eduQuestDecision": "Rejected"
+      }))
+})
+
+app.put("/api/contacts/4", (req,res)=>{
+    res.send(JSON.stringify(
+      {
+        "id": 4,
+        "firstName": "Bob",
+        "lastName": "Smith",
+        "email": "bob@example.com",
+        "age": 28,
+        "country": "United Kingdom",
+        "timezone": "GMT+00:00",
+        "sourceOfReferral": "Email",
+        "eduQuestSelectedDateTime": "2023-10-03T14:15:00Z",
+        "eduQuestDecision": "Rejected"
+      }))
+})
+
+app.delete("/api/contacts/1", (req,res)=>{
+    res.send(JSON.stringify({
+        "id": 1,
+        "firstName": "Alice",
+        "lastName": "Johnson",
+        "email": "alice@example.com",
+        "age": 30,
+        "country": "Canada",
+        "timezone": "GMT-04:00",
+        "sourceOfReferral": "Website",
+        "eduQuestSelectedDateTime": "2023-10-03T15:45:00Z",
+        "eduQuestDecision": "Pending"
+      }))
+})
+
+app.delete("/api/contacts/2", (req,res)=>{
+    res.send(JSON.stringify(
+      {
+        "id": 2,
+        "firstName": "Bob",
+        "lastName": "Smith",
+        "email": "bob@example.com",
+        "age": 28,
+        "country": "United Kingdom",
+        "timezone": "GMT+00:00",
+        "sourceOfReferral": "Email",
+        "eduQuestSelectedDateTime": "2023-10-03T14:15:00Z",
+        "eduQuestDecision": "Rejected"
+      }))
+})
+
+app.delete("/api/contacts/4", (req,res)=>{
+    res.send(JSON.stringify(
+      {
+        "id": 4,
+        "firstName": "Bob",
+        "lastName": "Smith",
+        "email": "bob@example.com",
+        "age": 28,
+        "country": "United Kingdom",
+        "timezone": "GMT+00:00",
+        "sourceOfReferral": "Email",
+        "eduQuestSelectedDateTime": "2023-10-03T14:15:00Z",
+        "eduQuestDecision": "Rejected"
+      }))
+})
+
+app.post("/contacts/bulking-creation", (req,res)=>{
+    res.send(JSON.stringify([{
+        "id": 1,
+        "firstName": "Alice",
+        "lastName": "Johnson",
+        "email": "alice@example.com",
+        "age": 30,
+        "country": "Canada",
+        "timezone": "GMT-04:00",
+        "sourceOfReferral": "Website",
+        "eduQuestSelectedDateTime": "2023-10-03T15:45:00Z",
+        "eduQuestDecision": "Pending"
+      },
+      {
+        "id": 2,
+        "firstName": "Bob",
+        "lastName": "Smith",
+        "email": "bob@example.com",
+        "age": 28,
+        "country": "United Kingdom",
+        "timezone": "GMT+00:00",
+        "sourceOfReferral": "Email",
+        "eduQuestSelectedDateTime": "2023-10-03T14:15:00Z",
+        "eduQuestDecision": "Rejected"
+      },
+      {
+        "id": 4,
+        "firstName": "Bob",
+        "lastName": "Smith",
+        "email": "bob@example.com",
+        "age": 28,
+        "country": "United Kingdom",
+        "timezone": "GMT+00:00",
+        "sourceOfReferral": "Email",
+        "eduQuestSelectedDateTime": "2023-10-03T14:15:00Z",
+        "eduQuestDecision": "Rejected"
+      }
+    ]))
+})
